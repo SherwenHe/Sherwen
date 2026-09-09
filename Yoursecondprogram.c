bc2134f2c4db402f1e0dc4393ca2f743207338bf
Sherwen <shiwen.he.hn@outlook.com>
2026-09-09 13:53:58 +0800

    Yoursecondprogram

---
+++ Yoursecondprogram.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    int n=2; //define the type of variable
+    printf("%d\n",n);
+    int a=2,b=3;//define the type of variable
+    printf("%d\n",a*b);
+    return 0;
+}
